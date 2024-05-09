65b13554ded93575fec9c23aa79e7dd3858338fb
Omri Cassiem <123456789+omri.i.cassiem@gmail.noreply.github.com>
2024-05-09 14:20:36 +0200

    added post and get endpoints to main.py

--- backend/api/src/main.py
+++ backend/api/src/main.py
@@ -3,3 +3,20 @@ from fastapi import FastAPI
 app = FastAPI()
 
 user = []
+
+
+@app.post('/register')
+def register():
+    return {}
+
+@app.post('/login')
+def login():
+    return {}
+
+@app.get('/unprotected')
+def unprotected():
+    return{"not" : "working"}
+
+@app.get('/protected')
+def protected():
+    return 
